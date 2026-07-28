c013836b257ed543db5aa63551e254c5995adab8
Musthia <fabioeduardo304@hotmail.com>
2026-07-27 22:54:00 -0300

    datcorr v6.4 railway1

--- backend/routers/usuarios_router.py
+++ backend/routers/usuarios_router.py
@@ -102,7 +102,7 @@ router = APIRouter(
 # -----------------------------------
 
 @router.get(
-    "",
+    "/",
     response_model=UsuariosListadoResponse
 )
 
@@ -312,7 +312,7 @@ def obtener_usuario_por_id(
 # -----------------------------------
 
 @router.post(
-    "",
+    "/",
     response_model=UsuarioCreateResponse
 )
 
